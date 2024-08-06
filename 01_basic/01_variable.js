02942ff77c660f371a3100cee68e7e404587d817
Raj Sanepara <117012155+Rajsanepara@users.noreply.github.com>
2024-08-06 17:25:39 +0000

    string

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -12,3 +12,14 @@ let nam="raj"
 //-------------------comparision------------
 //  console.table("2"==2)   // 
 //  console.table("2"===2)  // it is also cheak data type 
+
+// memory
+
+//stack(use for primitive),heap(use for non primitive)
+// console.log(`hello my name is ${nam} and my id number is ${p}`)
+
+// const gn = new String('megafast')
+// const gn='megafast'
+// console.log(gn[0])
+// console.log(gn.charAt(2))
+// console.log(gn.indexOf('g'))
