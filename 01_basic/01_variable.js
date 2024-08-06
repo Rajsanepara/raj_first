595795870372af4f4ca9538f9a040d614260ab5c
Raj Sanepara <117012155+Rajsanepara@users.noreply.github.com>
2024-08-06 16:04:18 +0000

    vidio 9

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+const p=10;
+let nam="raj"
+
+// console.log(p)
+// console.log(nam)
+
+// nam = "tirth"
+// console.table([p,nam])
+
+// console.log(typeof table)
+//-------------------operation------------ 
+//-------------------comparision------------
+//  console.table("2"==2)   // 
+//  console.table("2"===2)  // it is also cheak data type 
